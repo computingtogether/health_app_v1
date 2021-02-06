f77108939f98689f4f48d49a72f4b193c68b6a8d
computingtogether <computingtogether@gmail.com>
2021-02-06 17:17:53 -0500

    Add seed data, set widths of entries table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Entry.destroy_all
+
+user_entries = Entry.create([
+  {
+    foods: "almond butter, rice, chicken", 
+    activities: "sitting, standing",
+    feelings: "sad, calm",
+    pains: "upperback"
+  },
+  {
+    foods: "almond butter, apple, 3 eggs, plantain, olive oil, tuna, bread, meat loaf, pasta, orange, salad, pizza, milk, cheese", 
+    activities: "sitting, standing, walking",
+    feelings: "happy, calm",
+    pains: "head ache"
+  },
+  {
+    foods: "milk, cheese, spinach, toast, apple", 
+    activities: "sitting, biking",
+    feelings: "peacful",
+    pains: "lower back"
+  },
+  {
+    foods: "croissant, 2 eggs, tea, avocado", 
+    activities: "sitting, walking",
+    feelings: "happy",
+    pains: "head ache"
+  }
+])
+
+
+
+
+  # t.string "foods"
+  # t.string "activities"
+  # t.string "feelings"
+  # t.string "pains"
+  # t.datetime "created_at",
