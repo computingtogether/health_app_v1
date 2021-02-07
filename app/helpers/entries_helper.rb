f78b3f4a338dcb4348dbd3be7783f14573d7d25c
computingtogether <computingtogether@gmail.com>
2021-02-06 21:43:56 -0500

    Add table on entries page showing most eaten foods in descending order, add entries_helper module

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,2 +1,20 @@
 module EntriesHelper
+
+  def create_sorted_foods_tally
+    food_tally = {}
+
+    @entries.each do |entry|
+      food_array = entry.foods.split(',')
+      food_array.each do |food|
+        if(food_tally.include?(food))
+          food_tally[food] += 1
+        else
+          food_tally[food] = 1
+        end
+      end
+    end
+    food_tally = food_tally.sort_by{|k,v| -v}
+    food_tally.to_h
+  end
+
 end
